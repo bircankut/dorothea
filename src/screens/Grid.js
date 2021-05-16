57034999928bd535f060d8d8c6e1bccab633324f
bircankut <bircankut@gmail.com>
2021-05-16 14:07:57 +0300

    Add grid size that will be applied for the selected image

--- src/screens/Grid.js
+++ src/screens/Grid.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {
-  Image,
-  SafeAreaView,
+  ImageBackground,
   ScrollView,
   StyleSheet,
   Text,
@@ -12,46 +11,81 @@ import {
 import * as ImagePicker from 'react-native-image-picker';
 import Button from '../components/button';
 
+import Grid4 from '../assets/4x4Grid.svg';
+import Grid5 from '../assets/5x5Grid.svg';
+import Grid6 from '../assets/6x6Grid.svg';
+import Grid7 from '../assets/7x7Grid.svg';
+import Grid8 from '../assets/8x8Grid.svg';
+import Grid9 from '../assets/9x9Grid.svg';
+
 export default function Grid() {
   const [response, setResponse] = React.useState(null);
+  const [gridIndex, setGridIndex] = React.useState(0);
+  const grids = [
+    <View />,
+    <Grid4 witdh="100%" height="100%" />,
+    <Grid5 witdh="100%" height="100%" />,
+    <Grid6 witdh="100%" height="100%" />,
+    <Grid7 witdh="100%" height="100%" />,
+    <Grid8 witdh="100%" height="100%" />,
+    <Grid9 witdh="100%" height="100%" />,
+  ];
 
   return (
     <ScrollView style={{backgroundColor: '#FCF2F6'}}>
       <View style={styles.container}>
-        <View style={styles.image}>
+        <View style={styles.imageContainer}>
           {response && (
-            <Image
-              style={{width: 398, height: 398}}
-              source={{uri: response.uri}}
-            />
+            <ImageBackground
+              style={[{width: 398, height: 398}, styles.image]}
+              source={{uri: response.uri}}>
+              {grids[gridIndex]}
+            </ImageBackground>
           )}
         </View>
-        <ScrollView horizontal={true} style={styles.scrollContainer}   showsHorizontalScrollIndicator={false}>
-          <TouchableOpacity style={styles.sizeBox}>
+
+        <ScrollView
+          horizontal={true}
+          style={styles.scrollContainer}
+          showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity
+            style={styles.sizeBox}
+            onPress={() => setGridIndex(1)}>
             <Text>4 x 4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.sizeBox}>
+          <TouchableOpacity
+            style={styles.sizeBox}
+            onPress={() => setGridIndex(2)}>
             <Text>5 x 5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.sizeBox}>
+          <TouchableOpacity
+            style={styles.sizeBox}
+            onPress={() => setGridIndex(3)}>
             <Text>6 x 6</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.sizeBox}>
+          <TouchableOpacity
+            style={styles.sizeBox}
+            onPress={() => setGridIndex(4)}>
             <Text>7 x 7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.sizeBox}>
+          <TouchableOpacity
+            style={styles.sizeBox}
+            onPress={() => setGridIndex(5)}>
             <Text>8 x 8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.sizeBox}>
+          <TouchableOpacity
+            style={styles.sizeBox}
+            onPress={() => setGridIndex(6)}>
             <Text>9 x 9</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.sizeBox}>
-            <Text>10 x 10</Text>
           </TouchableOpacity>
         </ScrollView>
       </View>
 
-      <Button text={'Select'} style={styles.buttonStyle} />
+      <Button
+        text={'Hide Grid'}
+        style={styles.buttonStyle}
+        onPress={() => setGridIndex(0)}
+      />
       <Button
         text={'Select Image'}
         style={styles.buttonStyle}
@@ -85,7 +119,7 @@ const styles = StyleSheet.create({
     marginVertical: 24,
     marginHorizontal: 24,
   },
-  image: {
+  imageContainer: {
     marginTop: 10,
     marginHorizontal: 7,
     alignItems: 'center',
@@ -116,5 +150,11 @@ const styles = StyleSheet.create({
   buttonStyle: {
     marginHorizontal: 100,
     marginBottom: 20,
+  },
+  image: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
